6187551920c68ca7f9c054d556d4f83ef47db661
Elias Camacho Ramirez <eliascamachoramirez@outlook.com>
2024-12-06 14:39:18 -0600

    configuracion inicial

---
+++ src/test/java/prueba/tecnica/elias/camacho/ApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.tecnica.elias.camacho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
